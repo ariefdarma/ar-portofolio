b3ff9917aac95c94f5087b9a1cf8be9d84501b6e
crypt <arief.darma@proton.me>
2023-08-22 00:47:50 +0700

    card styling using tailwindcss

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -23,11 +23,16 @@ type ProjectProps = (typeof projectsData)[number];
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
     <section className="bh-gray-100 max-w-[42] border border-black/5 overflow-hidden sm:pr-8">
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h3 className="text-2xl font-semibold">{title}</h3>
+      <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
       <ul>
         {tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
+          <li
+            className="bg-black/0.7 px-3 py-1 text-[0.7] uppercase tracking-wider text-white rounded-full"
+            key={index}
+          >
+            {tag}
+          </li>
         ))}
       </ul>
       <Image src={imageUrl} alt="Project i work on" quality={95} />{' '}
