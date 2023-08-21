b32c61f0ff7b2e6b2b0996d20ea9e00c2632eed0
crypt <arief.darma@proton.me>
2023-08-22 00:52:06 +0700

    card phase 3

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -22,20 +22,25 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="bh-gray-100 max-w-[42] border border-black/5 overflow-hidden sm:pr-8">
+    <section className="bh-gray-100 max-w-[42] border border-black/5 overflow-hidden sm:pr-8 relative">
       <h3 className="text-2xl font-semibold">{title}</h3>
       <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-      <ul>
+      <ul className="flex flex-wrap mt-4">
         {tags.map((tag, index) => (
           <li
-            className="bg-black/0.7 px-3 py-1 text-[0.7] uppercase tracking-wider text-white rounded-full"
+            className="bg-black/[0.7] px-3 py-1 text-[0.7] uppercase tracking-wider text-white rounded-full"
             key={index}
           >
             {tag}
           </li>
         ))}
       </ul>
-      <Image src={imageUrl} alt="Project i work on" quality={95} />{' '}
+      <Image
+        src={imageUrl}
+        alt="Project i work on"
+        quality={95}
+        className="absolute top-8 -right-40"
+      />{' '}
     </section>
   );
 }
