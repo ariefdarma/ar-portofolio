42f6d343a9dfdf0967b552a0786f2d16e0986ecb
crypt <arief.darma@proton.me>
2023-08-21 23:28:15 +0700

    building card phase one

---
+++ components/Projects.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SectionHeading from './SectionHeading';
+import { projectsData } from '@/lib/Data';
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return <div></div>;
+}
