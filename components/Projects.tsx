b7b653a4587501cdeef77c746d09f5d6b3fab8ad
crypt <arief.darma@proton.me>
2023-08-22 00:41:02 +0700

    BUILDING CARD PHASE 2.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SectionHeading from './SectionHeading';
 import { projectsData } from '@/lib/Data';
+import Image from 'next/image';
 
 export default function Projects() {
   return (
@@ -20,5 +21,16 @@ export default function Projects() {
 type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return <div></div>;
+  return (
+    <section className="bh-gray-100 max-w-[42] border border-black/5 overflow-hidden sm:pr-8">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+      <Image src={imageUrl} alt="Project i work on" quality={95} />{' '}
+    </section>
+  );
 }
