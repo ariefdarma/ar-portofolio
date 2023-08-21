4552ef3f1956d9515eff4d00888f0677b5bf67d7
crypt <arief.darma@proton.me>
2023-08-21 21:35:16 +0700

    create section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from '@/components/Intro';
+import SectionDivider from '@/components/SectionDivider';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithub, FaGithubSquare } from 'react-icons/fa';
+import { group } from 'console';
 
 export default function Intro() {
   return (
@@ -69,18 +70,30 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full"
+          className="group  bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
         >
           Contact Me Here
-          <BsArrowRight />
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a className="bg-white px-7 py-3 flex items-center gap-2 rounded-full">
-          Download CV <HiDownload />
+        <a
+          className="group  bg-white px-7 py-3 flex items-center gap-2 rounded-full
+          outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/.pdf"
+          download
+        >
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:transition-y-1 transition" />
         </a>
-        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full  outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://linkedin.com"
+          target="_blank"
+        >
           <BsLinkedin />
         </a>
-        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+
+        <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full  outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10">
           <FaGithubSquare />
         </a>
       </motion.div>
